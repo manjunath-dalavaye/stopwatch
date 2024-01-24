f80e21ac8317a968cf74cd20885cd654a3367b0e
manjunath-dalavaye <manjunathadvg2002@gmail.com>
2024-01-25 01:16:31 +0530

    final_commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isRunning, setisRunning] = useState(false);
+  const [elapsedTime, setelapsedTime] = useState(0);
+
+  //For the time format
+  const formatTime = (secs) => {
+  const mins = Math.floor(secs / 60);
+  const remainingSecs = secs % 60;
+  return `${mins}:${remainingSecs < 10 ? "0" : ""}${remainingSecs}`
+  };
+
+  const toggleTime = () =>{
+    setisRunning((prevIsRunning) => !prevIsRunning);
+  };
+
+  const reset = () =>{
+    setisRunning(false);
+    setelapsedTime(0);
+  };
+
+  useEffect(() => {
+    let intervalId;
+    if(isRunning){
+      intervalId = setInterval(()=>{
+        setelapsedTime((prevElapsedTime) => prevElapsedTime + 1)
+      },1000);
+    }
+    else{
+      clearInterval(intervalId);
+    }
+    return()=> clearInterval(intervalId);
+  },[isRunning]);
+
   return (
     <div>
-      <h1>Stop Watch</h1>
+      <h1>Stopwatch</h1>
+      <p>Time: {formatTime(elapsedTime)}</p>
+      <button onClick={toggleTime}>{isRunning ? "Stop" : "Start"}</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
